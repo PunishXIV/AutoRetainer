47063e163709c6d8941b4f9c5f02a296cdfc1758
Limiana <5073202+Limiana@users.noreply.github.com>
2023-01-16 14:54:09 +0300

    Notify off by default

--- AutoRetainer/Config.cs
+++ AutoRetainer/Config.cs
@@ -64,7 +64,7 @@ internal class Config : IPluginConfiguration
     public bool NotifyCombatDutyNoDisplay = true;
     public bool NotifyIncludeAllChara = true;
     public bool NotifyIgnoreNoMultiMode = false;
-    public bool NotifyDisplayInChat = true;
+    public bool NotifyDisplayInChatX = false;
     public bool NotifyDeskopToast = false;
     public bool NotifyFlashTaskbar = false;
     public bool NotifyNoToastWhenRunning = true;
