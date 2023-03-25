8190028a23fd503994b1eb74487f0c85c20a7cea
Kane Valentine <kane@cute.im>
2023-03-25 03:21:52 +0000

    Update Beta.cs

--- AutoRetainer/UI/Settings/Beta.cs
+++ AutoRetainer/UI/Settings/Beta.cs
@@ -1,4 +1,4 @@
-﻿using Dalamud.Interface.Components;
+using Dalamud.Interface.Components;
 using PunishLib.ImGuiMethods;
 
 namespace AutoRetainer.UI.Settings;
@@ -7,18 +7,18 @@ internal static class Beta
 {
     internal static void Draw()
     {
-        ImGuiEx.Text(ImGuiColors.DalamudOrange, $"These features might be incomplete or cause minor problems.");
-        InfoBox.DrawBox("Retainer Sense", delegate
+        ImGuiEx.Text(ImGuiColors.DalamudOrange, $"These features might be incomplete, cause problems, or simply not work.");
+        InfoBox.DrawBox("RetainerSense", delegate
         {
-            ImGui.Checkbox("Retainer Sense", ref P.config.RetainerSense);
-            ImGuiComponents.HelpMarker($"Once you come near retainer bell and stay still for set amount of seconds, AutoRetainer will open the bell and enable itself if ventures are available");
+            ImGui.Checkbox("RetainerSense", ref P.config.RetainerSense);
+            ImGuiComponents.HelpMarker($"AutoRetainer will automatically enable itself when the player is within interaction range of a Summoning Bell. You must remain stationary or the activation will be cancelled.");
             ImGui.SetNextItemWidth(200f);
-            ImGuiEx.SliderIntAsFloat("Seconds to stay still before activation", ref P.config.RetainerSenseThreshold, 1000, 100000);
+            ImGuiEx.SliderIntAsFloat("Activation Time", ref P.config.RetainerSenseThreshold, 1000, 100000);
         });
         InfoBox.DrawBox("House Enter Task", delegate
         {
-            ImGui.Checkbox($"Multi Mode: support housing retainer bells", ref P.config.MultiAllowHET);
-            ImGuiEx.TextWrapped(ImGuiColors.DalamudOrange, $"Retainer bell must be within reach after you enter the house.");
+            ImGui.Checkbox($"Multi Mode: Housing Bell Support", ref P.config.MultiAllowHET);
+            ImGuiEx.TextWrapped(ImGuiColors.DalamudOrange, $"A Summoning Bell must be within range of the spawn point once the home is entered.");
         });
     }
 }
