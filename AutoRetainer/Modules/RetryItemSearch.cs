dd765c025ce3eec2994e64ac93345a1a43424397
Limiana <5073202+Limiana@users.noreply.github.com>
2023-05-03 15:11:32 +0300

    update

--- AutoRetainer/Modules/RetryItemSearch.cs
+++ AutoRetainer/Modules/RetryItemSearch.cs
@@ -17,13 +17,18 @@ namespace AutoRetainer.Modules
             {
                 if(TryGetAddonByName<AtkUnitBase>("ItemSearchResult", out var addon) && IsAddonReady(addon))
                 {
+                    if (EzThrottler.Throttle("log1")) InternalLog.Verbose("ItemSearchResult visible");
                     if (addon->UldManager.NodeList[25]->IsVisible)
                     {
+                        if (EzThrottler.Throttle("log2")) InternalLog.Verbose("Node 25 visible");
                         var t = MemoryHelper.ReadSeString(&addon->UldManager.NodeList[25]->GetAsAtkTextNode()->NodeText).ExtractText();
-                        if(t.EqualsAny(Utils.GetAddonText(1997), Utils.GetAddonText(1998)))
+                        if (EzThrottler.Throttle("log3")) InternalLog.Verbose("Node text is" + t);
+                        if (t.EqualsAny(Utils.GetAddonText(1997), Utils.GetAddonText(1998)))
                         {
+                            if (EzThrottler.Throttle("log4")) InternalLog.Verbose("ItemSearchResult visible");
                             if (!P.TaskManager.IsBusy && EzThrottler.Throttle("RetrySearch", 1000))
                             {
+                                P.DebugLog("Enqueueing ItemSearchResult retry");
                                 P.TaskManager.Enqueue(delegate
                                 {
                                     if (TryGetAddonByName<AtkUnitBase>("ItemSearchResult", out var a))
