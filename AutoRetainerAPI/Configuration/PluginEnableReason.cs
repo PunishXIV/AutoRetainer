f00311962dab57d8dee8dab0fc7b9452a3adde7d
Limiana <5073202+Limiana@users.noreply.github.com>
2023-10-19 15:42:11 +0300

    move api to other repo

--- AutoRetainerAPI/Configuration/PluginEnableReason.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace AutoRetainerAPI.Configuration;
-
-[Obfuscation(Exclude = true, ApplyToMembers = true)]
-public enum PluginEnableReason
-{
-    Access, Manual, Auto, MultiMode, Artisan
-}
