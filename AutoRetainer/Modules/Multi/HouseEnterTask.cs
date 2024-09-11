c7fe2b72be830760766c29cafae9a369b4c30750
Limiana <5073202+Limiana@users.noreply.github.com>
2024-09-11 08:33:32 +0300

    Remove old code

--- AutoRetainer/Modules/Multi/HouseEnterTask.cs
+++ AutoRetainer/Modules/Multi/HouseEnterTask.cs
@@ -12,69 +12,6 @@ namespace AutoRetainer.Modules.Multi;
 
 internal static unsafe class HouseEnterTask
 {
-    private static void EnqueueTask()
-    {
-        PluginLog.Debug($"Enqueued HouseEnterTask from {new StackTrace().GetFrames().Select(x => x.GetMethod()?.Name).Prepend("      ").Print("\n")}");
-        P.TaskManager.Enqueue(NewYesAlreadyManager.WaitForYesAlreadyDisabledTask);
-        P.TaskManager.Enqueue(WaitUntilNotBusy, new(timeLimitMS:180 * 1000));
-        P.TaskManager.Enqueue(() =>
-        {
-            P.TaskManager.BeginStack();
-            try
-            {
-                if(Utils.GetReachableRetainerBell(false) == null)
-                {
-                    var entrance = Utils.GetNearestEntrance(out var d);
-                    var validDistance = 4f;
-                    if(entrance != null && d > validDistance)
-                    {
-                        P.TaskManager.Enqueue(() => SetTarget(40f));
-                        P.TaskManager.Enqueue(Lockon);
-                        P.TaskManager.Enqueue(Approach);
-                        P.TaskManager.Enqueue(AutorunOff);
-                        P.TaskManager.Enqueue(() => { Chat.Instance.ExecuteCommand("/automove off"); });
-                    }
-                    else if(entrance == null)
-                    {
-                        return null;
-                    }
-                    P.TaskManager.Enqueue(() => SetTarget(5f));
-                    P.TaskManager.Enqueue(Interact);
-                    P.TaskManager.Enqueue(SelectYesno);
-                    P.TaskManager.Enqueue(WaitUntilLeavingZone);
-                    P.TaskManager.EnqueueDelay(60, true);
-                    P.TaskManager.Enqueue(Utils.WaitForScreen);
-                }
-            }
-            catch(Exception e) { e.Log(); }
-            P.TaskManager.InsertStack();
-            return true;
-        }, "Master HET");
-        //TaskContinueHET.Enqueue();
-    }
-
-    private static bool? WaitUntilNotBusy()
-    {
-        if(!IsScreenReady()) return false;
-        if(!ProperOnLogin.PlayerPresent || !ResidentalAreas.List.Contains(Svc.ClientState.TerritoryType)) return null;
-        if(MultiMode.IsInteractionAllowed())
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private static bool? Lockon()
-    {
-        var entrance = Utils.GetNearestEntrance(out _);
-        if(entrance != null && Svc.Targets.Target?.Address == entrance.Address && EzThrottler.Throttle("HET.Lockon"))
-        {
-            Chat.Instance.ExecuteCommand("/lockon");
-            return true;
-        }
-        return false;
-    }
-
     internal static bool? Approach()
     {
         DebugLog($"Enabling automove");
@@ -82,44 +19,6 @@ internal static unsafe class HouseEnterTask
         Chat.Instance.ExecuteCommand("/automove on");
         return true;
     }
-
-    internal static bool? AutorunOff()
-    {
-        var entrance = Utils.GetNearestEntrance(out var d);
-        if(entrance != null && d < 3f + Utils.Random && EzThrottler.Throttle("HET.DisableAutomove"))
-        {
-            DebugLog($"Disabling automove");
-            Chat.Instance.ExecuteCommand("/automove off");
-            return true;
-        }
-        return false;
-    }
-
-
-    internal static bool? SetTarget(float distance)
-    {
-        var entrance = Utils.GetNearestEntrance(out var d);
-        if(entrance != null && d < distance && EzThrottler.Throttle("HET.SetTarget", 200))
-        {
-            DebugLog($"Setting entrance target ({distance})");
-            Svc.Targets.Target = (entrance);
-            return true;
-        }
-        return false;
-    }
-
-    internal static bool? Interact()
-    {
-        var entrance = Utils.GetNearestEntrance(out var d);
-        if(entrance != null && Svc.Targets.Target?.Address == entrance.Address && EzThrottler.Throttle("HET.Interact", 1000))
-        {
-            DebugLog($"Interacting with entrance");
-            TargetSystem.Instance()->InteractWithObject((FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)entrance.Address, false);
-            return true;
-        }
-        return false;
-    }
-
 
     internal static bool? SelectYesno()
     {
--- AutoRetainer/Modules/Voyage/Tasks/TaskContinueHET.cs
+++ AutoRetainer/Modules/Voyage/Tasks/TaskContinueHET.cs
@@ -7,108 +7,12 @@ namespace AutoRetainer.Modules.Voyage.Tasks;
 
 internal static unsafe class TaskContinueHET
 {
-    private static void Enqueue()
-    {
-        VoyageUtils.Log($"Task enqueued: {nameof(TaskContinueHET)}");
-        P.TaskManager.Enqueue(NpcSaleManager.EnqueueIfItemsPresent);
-        P.TaskManager.Enqueue(Utils.WaitForScreen);
-        P.TaskManager.Enqueue(() => !IsOccupied(), "WaitUntilNotOccupied1", new(timeLimitMS:180 * 1000));
-        P.TaskManager.Enqueue(() =>
-        {
-            if(VoyageUtils.ShouldEnterWorkshop())
-            {
-                if(Utils.GetNearestWorkshopEntrance(out _) != null)
-                {
-                    EnqueueImmediateEnterWorkshop();
-                }
-            }
-        });
-    }
-
-    private static void EnqueueImmediateEnterWorkshop()
-    {
-        P.TaskManager.BeginStack();
-        P.TaskManager.Enqueue(() => !IsOccupied() && IsScreenReady(), "WaitUntilNotOccupied2", new(timeLimitMS:180 * 1000));
-        P.TaskManager.Enqueue(LockonAdditionalChambers, new(timeLimitMS:1000, abortOnTimeout:true));
-        P.TaskManager.Enqueue(HouseEnterTask.Approach);
-        P.TaskManager.Enqueue(AutorunOffAdd);
-        P.TaskManager.Enqueue(() => { Chat.Instance.ExecuteCommand("/automove off"); });
-        P.TaskManager.Enqueue(InteractAdd);
-        P.TaskManager.Enqueue(SelectEnterWorkshop);
-        P.TaskManager.Enqueue(() => VoyageUtils.Workshops.Contains(Svc.ClientState.TerritoryType), "Wait Until entered workshop");
-        P.TaskManager.EnqueueDelay(60, true);
-        P.TaskManager.Enqueue(Utils.WaitForScreen);
-        P.TaskManager.InsertStack();
-    }
-
-    private static void EnqueueEnterWorkshop()
-    {
-        P.TaskManager.Enqueue(() => !IsOccupied() && IsScreenReady(), "WaitUntilNotOccupied2", new(timeLimitMS:180 * 1000));
-        P.TaskManager.Enqueue(LockonAdditionalChambers, new(timeLimitMS:1000, abortOnTimeout:true));
-        P.TaskManager.Enqueue(HouseEnterTask.Approach);
-        P.TaskManager.Enqueue(AutorunOffAdd);
-        P.TaskManager.Enqueue(() => { Chat.Instance.ExecuteCommand("/automove off"); });
-        P.TaskManager.Enqueue(InteractAdd);
-        P.TaskManager.Enqueue(SelectEnterWorkshop);
-        P.TaskManager.Enqueue(() => VoyageUtils.Workshops.Contains(Svc.ClientState.TerritoryType), "Wait Until entered workshop");
-        P.TaskManager.EnqueueDelay(60, true);
-        P.TaskManager.Enqueue(Utils.WaitForScreen);
-    }
-
     internal static bool? SelectEnterWorkshop()
     {
         if(Utils.TrySelectSpecificEntry(Lang.EnterWorkshop, () => EzThrottler.Throttle("HET.SelectEnterWorkshop")))
         {
             DebugLog("Confirmed going to workhop");
             return true;
-        }
-        return false;
-    }
-
-    internal static bool? InteractAdd()
-    {
-        var entrance = Utils.GetNearestWorkshopEntrance(out var d);
-        if(entrance != null && Svc.Targets.Target?.Address == entrance.Address && EzThrottler.Throttle("HET.InteractAdd", 1000))
-        {
-            DebugLog($"Interacting with entrance");
-            TargetSystem.Instance()->InteractWithObject((FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)entrance.Address, false);
-            return true;
-        }
-        return false;
-    }
-    internal static bool? AutorunOffAdd()
-    {
-        var entrance = Utils.GetNearestWorkshopEntrance(out var d);
-        if(entrance != null && d < 3f + Utils.Random && EzThrottler.Throttle("HET.DisableAutomoveAdd"))
-        {
-            DebugLog($"Disabling automove");
-            Chat.Instance.ExecuteCommand("/automove off");
-            return true;
-        }
-        return false;
-    }
-
-    internal static bool? LockonAdditionalChambers()
-    {
-        var entrance = Utils.GetNearestWorkshopEntrance(out _);
-        if(entrance != null)
-        {
-            if(Svc.Targets.Target?.Address == entrance.Address)
-            {
-                if(EzThrottler.Throttle("HET.LockonAdd"))
-                {
-                    Chat.Instance.ExecuteCommand("/lockon");
-                    return true;
-                }
-            }
-            else
-            {
-                if(EzThrottler.Throttle("HET.SetTargetAdd", 200))
-                {
-                    DebugLog($"Setting entrance target ({entrance})");
-                    Svc.Targets.Target = entrance;
-                }
-            }
         }
         return false;
     }
