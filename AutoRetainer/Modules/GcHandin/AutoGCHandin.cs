0ed30cadac8a9e5f507fbcfb580ba82fa7c3ac18
Limiana <5073202+Limiana@users.noreply.github.com>
2026-04-30 05:02:00 +0300

    7.5

--- AutoRetainer/Modules/GcHandin/AutoGCHandin.cs
+++ AutoRetainer/Modules/GcHandin/AutoGCHandin.cs
@@ -249,7 +249,7 @@ internal static unsafe class AutoGCHandin
                 GCSupplyListAddon != null
                 && IsAddonReady(GCSupplyListAddon)
                 && GCSupplyListAddon->UldManager.NodeListCount > 20
-                && GCSupplyListAddon->UldManager.NodeList[5]->IsVisible()
+                && GCSupplyListAddon->UldManager.SearchNodeById(24)->IsVisible()
                 && IsSelectedFilterValid(GCSupplyListAddon);
         }
         catch(Exception)
@@ -259,11 +259,11 @@ internal static unsafe class AutoGCHandin
     }
     internal static bool IsDone(AtkUnitBase* addon)
     {
-        return addon->UldManager.NodeList[20]->IsVisible();
+        return addon->UldManager.SearchNodeById(9)->IsVisible();
     }
     internal static bool IsSelectedFilterValid(AtkUnitBase* addon)
     {
-        var step1 = addon->UldManager.NodeList[14];
+        var step1 = addon->UldManager.SearchNodeById(14);
         var step2 = step1->GetAsAtkComponentNode()->Component->UldManager.NodeList[1];
         var step3 = step2->GetAsAtkComponentNode()->Component->UldManager.NodeList[2];
         var text = GenericHelpers.ReadSeString(&step3->GetAsAtkTextNode()->NodeText).GetText();
